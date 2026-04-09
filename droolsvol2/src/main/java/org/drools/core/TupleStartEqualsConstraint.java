87da8091d74cc460feb3db0886cdaba8a0c3d0c5
Mark Proctor <mdproctor@gmail.com>
2026-04-09 06:13:30 +0100

    fix(droolsvol2): implement all BetaConstraint methods in TupleStartEqualsConstraint
    Refs #6648

--- droolsvol2/src/main/java/org/drools/core/TupleStartEqualsConstraint.java
+++ droolsvol2/src/main/java/org/drools/core/TupleStartEqualsConstraint.java
@@ -1,8 +1,14 @@
 package org.drools.core;
 import org.drools.base.rule.constraint.BetaConstraint;
+import org.kie.api.runtime.rule.FactHandle;
+import org.drools.base.reteoo.BaseTuple;
 /** TODO #6650: Temporary stub. */
-public class TupleStartEqualsConstraint implements BetaConstraint {
+@SuppressWarnings("unchecked")
+public class TupleStartEqualsConstraint implements BetaConstraint<Object> {
     private static final TupleStartEqualsConstraint INSTANCE = new TupleStartEqualsConstraint();
     public static TupleStartEqualsConstraint getInstance() { return INSTANCE; }
-    @Override public BetaConstraint cloneIfInUse() { return this; }
+    @Override public boolean isAllowedCachedLeft(Object context, FactHandle handle) { return true; }
+    @Override public boolean isAllowedCachedRight(BaseTuple tuple, Object context) { return true; }
+    @Override public Object createContext() { return null; }
+    @Override public BetaConstraint<Object> cloneIfInUse() { return this; }
 }
