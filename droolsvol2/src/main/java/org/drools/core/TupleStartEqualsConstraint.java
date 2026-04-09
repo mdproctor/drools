a511ae081233db6fd38f6703ef0c4d656d34ccf3
Mark Proctor <mdproctor@gmail.com>
2026-04-09 06:16:22 +0100

    fix(droolsvol2): implement all Constraint interface methods in TupleStartEqualsConstraint
    Refs #6648

--- droolsvol2/src/main/java/org/drools/core/TupleStartEqualsConstraint.java
+++ droolsvol2/src/main/java/org/drools/core/TupleStartEqualsConstraint.java
@@ -1,7 +1,12 @@
 package org.drools.core;
+import org.drools.base.rule.Declaration;
 import org.drools.base.rule.constraint.BetaConstraint;
-import org.kie.api.runtime.rule.FactHandle;
+import org.drools.base.rule.constraint.Constraint;
 import org.drools.base.reteoo.BaseTuple;
+import org.kie.api.runtime.rule.FactHandle;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 /** TODO #6650: Temporary stub. */
 @SuppressWarnings("unchecked")
 public class TupleStartEqualsConstraint implements BetaConstraint<Object> {
@@ -12,4 +17,10 @@ public class TupleStartEqualsConstraint implements BetaConstraint<Object> {
     @Override public Object createContext() { return null; }
     @Override public BetaConstraint<Object> cloneIfInUse() { return this; }
     @Override public boolean isTemporal() { return false; }
+    @Override public Declaration[] getRequiredDeclarations() { return new Declaration[0]; }
+    @Override public void replaceDeclaration(Declaration oldDecl, Declaration newDecl) { }
+    @Override public Constraint clone() { return this; }
+    @Override public Constraint.ConstraintType getType() { return Constraint.ConstraintType.BETA; }
+    @Override public void writeExternal(ObjectOutput out) throws IOException { }
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException { }
 }
