88300879c3bd630b456d494324a90738a4b93a4f
Mark Proctor <mdproctor@gmail.com>
2026-04-09 06:12:17 +0100

    fix(droolsvol2): TupleStartEqualsConstraint implement cloneIfInUse()
    Refs #6648

--- droolsvol2/src/main/java/org/drools/core/TupleStartEqualsConstraint.java
+++ droolsvol2/src/main/java/org/drools/core/TupleStartEqualsConstraint.java
@@ -4,4 +4,5 @@ import org.drools.base.rule.constraint.BetaConstraint;
 public class TupleStartEqualsConstraint implements BetaConstraint {
     private static final TupleStartEqualsConstraint INSTANCE = new TupleStartEqualsConstraint();
     public static TupleStartEqualsConstraint getInstance() { return INSTANCE; }
+    @Override public BetaConstraint cloneIfInUse() { return this; }
 }
